1c269e0ca2bf8780ab436565991e707f0b7aef57
Igbineweka Vincent <ifyvinz@gmail.com>
2025-01-24 00:16:48 +0100

    files in ec2

--- scripts/remove_existing_start_docker.sh
+++ scripts/remove_existing_start_docker.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-if [ -e /home/ec2-user/app/scripts/start_docker.sh ]; then
+
+# Check if start_docker.sh exists and is a directory, then remove it
+if [ -d /home/ec2-user/app/scripts/start_docker.sh ]; then
     sudo rm -rf /home/ec2-user/app/scripts/start_docker.sh
+elif [ -f /home/ec2-user/app/scripts/start_docker.sh ]; then
+    sudo rm -f /home/ec2-user/app/scripts/start_docker.sh
 fi
 
-if [ -e /home/ec2-user/app/scripts/docker_compose_up.sh ]; then
+# Check if docker_compose_up.sh exists and is a directory, then remove it
+if [ -d /home/ec2-user/app/scripts/docker_compose_up.sh ]; then
     sudo rm -rf /home/ec2-user/app/scripts/docker_compose_up.sh
+elif [ -f /home/ec2-user/app/scripts/docker_compose_up.sh ]; then
+    sudo rm -f /home/ec2-user/app/scripts/docker_compose_up.sh
 fi
