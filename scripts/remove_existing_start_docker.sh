8fd85715d655158b005a54c2c0257595c5d67504
Igbineweka Vincent <ifyvinz@gmail.com>
2025-01-24 00:12:02 +0100

    before install script

--- scripts/remove_existing_start_docker.sh
+++ scripts/remove_existing_start_docker.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-rm -f /home/ec2-user/app/scripts/start_docker.sh
-rm -f /home/ec2-user/app/scripts/docker_compose_up.sh
+if [ -e /home/ec2-user/app/scripts/start_docker.sh ]; then
+    sudo rm -rf /home/ec2-user/app/scripts/start_docker.sh
+fi
+
+if [ -e /home/ec2-user/app/scripts/docker_compose_up.sh ]; then
+    sudo rm -rf /home/ec2-user/app/scripts/docker_compose_up.sh
+fi
