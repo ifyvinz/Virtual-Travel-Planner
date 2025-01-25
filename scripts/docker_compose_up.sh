674cfcea28a5136eb6fafcabaaf848fec13a2e73
Igbineweka Vincent <ifyvinz@gmail.com>
2025-01-25 15:36:47 +0100

    index.html

--- scripts/docker_compose_up.sh
+++ scripts/docker_compose_up.sh
@@ -2,22 +2,23 @@
 
 cd /home/ec2-user/app
 
+# Pull the latest code from the repository
+#echo "Pulling the latest code from the repository..."
+#git reset --hard  # Reset any local changes
+#git pull origin main  # Update to the latest code 
+
 # Stop existing services
 echo "Stopping existing services..."
 docker-compose down
 
-# Pull the latest image
+# Pull the latest Docker image
 echo "Pulling the latest Docker image..."
 docker-compose pull --ignore-pull-failures --no-cache
-
-# List available images (for debugging)
-echo "Listing Docker images..."
-docker images
 
 # Start fresh
 echo "Starting the application..."
 docker-compose up -d
 
-# Check running containers (for debugging)
+# Check running containers
 echo "Running containers..."
 docker ps
